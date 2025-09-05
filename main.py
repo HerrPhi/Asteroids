b954504ea9688d004e77587073724bda5a279be3
HerrPhi <fingatta@gmail.com>
2025-09-05 14:11:57 +0200

    added FPS to the game loop

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()   #initialised Clock object
+    dt = 0  #delta t variable
+
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
         screen.fill("black")
 
 
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
